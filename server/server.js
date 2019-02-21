ec11d115fdaa7e537081f0b98671e6b9b30497c9
Michal <mmatynia@gmail.com>
2019-02-21 19:01:48 +0100

    new branch2

--- server/server.js
+++ server/server.js
@@ -15,6 +15,8 @@ const { auth} = require('./middleware/auth')
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.use(express.static('client/build'));
+
 // GET //
 app.get('/api/auth',auth,(req,res)=>{
     res.json({
@@ -155,8 +157,15 @@ app.delete('/api/delete_book',(req,res)=>{
     })
 })
 
+if(process.env.NODE_ENV === 'production'){
+    const path = require('path');
+    app.get('/*',(req,res)=>{
+        res.sendfile(path.resolve(__dirname, '../client','build','index.html'))
+    })
+}
 
 const port = process.env.PORT || 3001;
 app.listen(port,()=>{
     console.log(`SERVER RUNNNING`)
 })
+
